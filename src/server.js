0287ca06d00cb589ed7e66daa20e03b46c3e6f69
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-11-22 17:56:26 -0300

    feat: static GET handler

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-// Welcome to the tutorial!
+import { createServer } from "miragejs"
+
+export default function () {
+    createServer({
+        // Definindo as rotas da nossa API simulada
+        routes() { 
+            // this.get() é o método  que nos permite simular as requisições GET
+            this.get("/api/reminders", () => ({
+                reminders: [
+                    {id: 1, text: "Walk the dog"},
+                    {id: 2, text: "Buy groceries"},
+                    {id: 3, text: "Take out the trash"},
+                    {id: 4, text: "Work out"},
+                ],
+            }))
+        }
+    })
+}
