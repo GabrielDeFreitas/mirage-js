b3b342aa36cbcb336d5d7b752c3b437ba6318c44
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-11-22 18:41:37 -0300

    feat: dynamic handlers

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
-import { createServer } from "miragejs"
+import { createServer, Model } from "miragejs"
 
 export default function () {
     createServer({
+        models: {
+            reminder: Model,
+        },
         // Definindo as rotas da nossa API simulada
         routes() { 
             // this.get() é o método  que nos permite simular as requisições GET
-            this.get("/api/reminders", () => ({
-                reminders: [
-                    {id: 1, text: "Walk the dog"},
-                    {id: 2, text: "Buy groceries"},
-                    {id: 3, text: "Take out the trash"},
-                    {id: 4, text: "Work out"},
-                ],
-            }))
-            let newId = 5
+            this.get("/api/reminders", (schema) => {
+                return schema.reminders.all()
+            })
             this.post("/api/reminders", (schema, request) => {
                 let attrs = JSON.parse(request.requestBody)
-                attrs.id = newId++
-
-                return { reminder: attrs }
+                return schema.reminders.create(attrs)
             })
         }
     })
