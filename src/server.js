52842d7e32254f30934ba6275fc0f4c1c47b2d2d
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-11-22 18:58:36 -0300

    feat: add seeds

--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ export default function () {
         models: {
             reminder: Model,
         },
+        // hook para inicializar o mirage com alguns dados iniciais
+        seeds(server) {
+            server.create("reminder", { text: "Walk the dog"})
+            server.create("reminder", { text: "Take out the trash"})
+            server.create("reminder", { text: "Work out"})
+        },
         // Definindo as rotas da nossa API simulada
         routes() { 
             // this.get() é o método  que nos permite simular as requisições GET
